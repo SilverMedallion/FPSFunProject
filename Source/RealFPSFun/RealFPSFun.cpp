390e2cfce908d8cedf388ab131700a15d64acdbf
SilverMedallion <liamlaverty6@gmail.com>
2025-02-25 23:15:09 +0000

    added c++ class for projectile to generate .sln file

---
+++ Source/RealFPSFun/RealFPSFun.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RealFPSFun.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RealFPSFun, "RealFPSFun" );
